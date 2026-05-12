8ea11e8a89ae3cdb8f843e7bfa5a16da8b81395b
Sanjar Atamuradov <sanjar.techie@gmail.com>
2026-05-11 18:13:47 -0700

    [#main] update requirements

--- src/main.py
+++ src/main.py
@@ -2,10 +2,9 @@ from __future__ import annotations
 
 import logging
 
-from humanola import robo
-
 from battery import PiperBattery
 from controllers import PiperXr
+from humanola import robo
 from piper_native import PiperSetupState, PiperSolver, PiperSolverConfig, PiperState
 from sources import PiperDataSource
 
@@ -21,20 +20,26 @@ if __name__ == "__main__":
         datefmt="%H:%M:%S",
     )
     state = PiperSetupState(
-        left_arm=PiperState.connect("can1"),
-        right_arm=PiperState.connect("can0"),
+        left_arm=PiperState.connect("can0"),
+        right_arm=PiperState.connect("can1"),
         solver=PiperSolver(config=PiperSolverConfig()),
     )
     channel, runtime = (
         robo.Robo(
             url="https://grpc.humanola.com",
-            api_key="<YOUR_API_KEY>",
-            robo_id="<YOUR_ROBO_ID>",
+            api_key="I3MObARleuenXMhRo4gATDG5tBrSYk11Su03YWxO",
+            robo_id="019cb288-a9b3-7dc3-9249-d8e7bd936549",
         )
+        # robo.Robo(
+        #     url="http://192.168.1.119:8001",
+        #     api_key="Fe1J3LXWZafMLEzND0QN0hzKLP452Mij7DMQpDfU",
+        #     robo_id="019c529b-0bab-7db0-af4c-20a87b97cbd1",
+        # )
         .add_controller("controller", PiperXr(state))
         .set_battery(PiperBattery())
         .add_source("data", PiperDataSource(state))
         .auto_discover_cameras()
+        .verbose()
         .run(on_error)
     )
     runtime.wait_for_interrupt()
